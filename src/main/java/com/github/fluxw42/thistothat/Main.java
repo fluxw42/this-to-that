480f69be57766aa1604a0385ed91ddfc36fb4233
Jeroen Meulemeester <jeroen.meulemeester@gmail.com>
2016-02-14 16:23:04 +0100

    Use the new location of the default config file

--- src/main/java/com/github/fluxw42/thistothat/Main.java
+++ src/main/java/com/github/fluxw42/thistothat/Main.java
@@ -11,9 +11,14 @@ import java.io.File;
  */
 public class Main {
 
+    /**
+     * The default config file
+     */
+    private static final File DEFAULT_CONFIG = new File("conf", "this-to-that.properties");
+
     public static void main(final String[] args) throws Exception {
         final File configFile = args == null || args.length == 0 || args[0] == null || args[0].trim().isEmpty() ?
-                new File("this-to-that.properties") : new File(args[0].trim());
+                DEFAULT_CONFIG : new File(args[0].trim());
 
         final ConfigurationImpl config = new ConfigurationImpl(configFile);
         final ThisToThat thisToThat = new ThisToThat(config);
